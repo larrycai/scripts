e3519b5d62a414e95afdd185f4bae3bb560f2f27
Larry Cai <larry.caiyu@gmail.com>
2012-02-11 21:21:23 +0800

    update for GPL license

--- pdfbokmark/pdfbokmark.rb
+++ pdfbokmark/pdfbokmark.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
-
 #
-# the count check solution and unicode inside pdf
-# are copied from W.Trevor King http://www.physics.drexel.edu/~wking/unfolding-disasters/posts/PDF_bookmarks_with_Ghostscript/
+# Copyright (C) 2011-2012 Larry Cai <larry.caiyu@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# Acknowledgement:
+# the solution of counting check and unicode handling for pdf metadata
+# is learned from W.Trevor King http://www.physics.drexel.edu/~wking/unfolding-disasters/posts/PDF_bookmarks_with_Ghostscript/
 
 require 'optparse'
 
