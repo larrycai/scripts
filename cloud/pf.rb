6e119693ea319036e9ece3a34659a03e5f90ff48
ehonlia <hongxin.liang@ericsson.com>
2012-02-24 16:30:27 +0800

    add status

--- cloud/pf.rb
+++ cloud/pf.rb
@@ -4,11 +4,15 @@ require 'optparse'
 require 'ostruct'
 
 PORT_MAP = {
-  220 => [22, :ssh],
-  308 => [3080, :http],
-  330 => [3300, :cai],
-  198 => [8998, :cai3g]
+  "220" => ["22", :ssh],
+  "308" => ["3080", :http],
+  "330" => ["3300", :cai],
+  "198" => ["8998", :cai3g]
 }
+
+def ps_command(ip, seq)
+  "ps -ewwf | egrep 'ssh .*[0-9].*#{seq}:#{ip}:[0-9]+' | grep -v grep"
+end
 
 def start(ip, seq)
   command = "ssh"
@@ -22,34 +26,47 @@ def start(ip, seq)
 end
 
 def stop(ip, seq)
-  # hope we kill the right people
-  command = "ps -elf | egrep 'ssh .*[0-9].*#{seq}:#{ip}:[0-9]+'" +
-    " | grep -v grep | awk '{print $4}' | xargs kill -9 > /dev/null 2>&1"
+  # hope we're killing the right people
+  command = "#{ps_command(ip, seq)} | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1"
 
   successful = system(command)
   yield successful if block_given?
+end
+
+def status(ip, seq)
+  output = `#{ps_command(ip, seq)}`
+  ports = []
+  output.split(" ").grep /-L(.*)/ do
+    ports << $1.split(":")[0]
+  end
+
+  ports.each do |p|
+    entry = PORT_MAP.find { |e| p.include?(e[0]) }
+    puts "#{entry[1][1].to_s}:#{" " * (10 - entry[1][1].to_s.size)} #{p}" if entry
+  end
 end
 
 def main
   options = OpenStruct.new
 
   option_parser = OptionParser.new do |opts|
-    opts.banner = "Usage: pf.rb -i, --ip <ip> -s, --seq <seq> {start|stop}"
+    opts.banner = "Usage: pf.rb -i, --ip <ip> [-s, --seq <seq>] {start|stop|status}"
 
     opts.on("-i", "--ip ip", "ip address of the instance") do |ip|
       options.ip = ip
     end
 
-    opts.on("-s", "--seq seq", "sequence number used as port suffix") do |seq|
+    opts.on("-s", "--seq [seq]", "sequence number used as port suffix") do |seq|
       options.seq = seq
     end
   end
 
   option_parser.parse!
 
-  if not options.ip or not options.seq
+  if not options.ip
     puts option_parser.help
   else
+    options.seq ||= options.ip.split(".")[-1]
     case ARGV[0]
     when "start" then start(options.ip, options.seq) do |successful, seq|
         if successful
@@ -67,6 +84,7 @@ def main
           puts "failed to stop port forwarding"
         end
       end
+    when "status" then status(options.ip, options.seq)
     else puts option_parser.help
     end
   end
