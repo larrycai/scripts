c969c3836ed937864baf875eda6b4f80320371ee
Honnix Liang <hxliang1982@gmail.com>
2012-03-05 21:47:51 +0800

    use erb to simply

--- cloud/reload_lb.rb
+++ cloud/reload_lb.rb
@@ -2,27 +2,25 @@
 
 require 'eventmachine'
 require 'open-uri'
+require 'erb'
 
 URL = "http://192.168.4.1:9393/app"
 CFG_FILE = "/etc/haproxy/haproxy.cfg"
-TEMPLATE_CFG_FILE = CFG_FILE + ".template"
+TEMPLATE_CFG_FILE = CFG_FILE + ".erb"
 
 EM.run do
   instances = []
 
-  EventMachine::PeriodicTimer.new(1) do
+  EventMachine::PeriodicTimer.new(5) do
     current_instances = open(URL).readline.split(",")
     unless instances == current_instances
       instances = current_instances
-      File.open(TEMPLATE_CFG_FILE, "r") do |template_cfg_file|
-        content = template_cfg_file.readlines.join("")
-        sub_cai_content = instances.map { |instance| "server #{instance} #{instance}:3300 check" }.join("\n\t")
-        sub_cai3g_content = instances.map { |instance| "server #{instance} #{instance}:8998 check" }.join("\n\t")
-        content.gsub!("\${cai}", sub_cai_content)
-        content.gsub!("\${cai3g}", sub_cai3g_content)
 
-        File.open(CFG_FILE, "w") { |cfg_file| cfg_file.puts content }
-      end
+      cai = instances.map { |instance| "server #{instance} #{instance}:3300 check" }.join("\n\t")
+      cai3g = instances.map { |instance| "server #{instance} #{instance}:8998 check" }.join("\n\t")
+
+      template = ERB.new(File.read(TEMPLATE_CFG_FILE))
+      File.write(CFG_FILE, template.result(binding))
 
       `/etc/init.d/haproxy reload`
 
