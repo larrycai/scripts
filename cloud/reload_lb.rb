db5219411c881f0fff35d106532ffb4b06f3c7dc
ehonlia <hongxin.liang@ericsson.com>
2012-03-20 10:04:21 +0800

    daemonize it

--- cloud/reload_lb.rb
+++ cloud/reload_lb.rb
@@ -3,28 +3,37 @@
 require 'eventmachine'
 require 'open-uri'
 require 'erb'
+require 'daemons'
+
+Daemons.daemonize
 
 URL = "http://192.168.4.1:9393/app"
 CFG_FILE = "/etc/haproxy/haproxy.cfg"
 TEMPLATE_CFG_FILE = CFG_FILE + ".erb"
 
+ENV["http_proxy"] = nil
+
 EM.run do
   instances = []
 
   EventMachine::PeriodicTimer.new(5) do
-    current_instances = open(URL).readline.split(",")
-    unless instances == current_instances
-      instances = current_instances
+    begin
+      current_instances = open(URL).readline.split(",")
+      unless instances == current_instances
+        instances = current_instances
 
-      cai = instances.map { |instance| "server #{instance} #{instance}:3300 check" }.join("\n\t")
-      cai3g = instances.map { |instance| "server #{instance} #{instance}:8998 check" }.join("\n\t")
+        cai = instances.map { |instance| "server #{instance} #{instance}:3300 check" }.join("\n\t")
+        cai3g = instances.map { |instance| "server #{instance} #{instance}:8998 check" }.join("\n\t")
 
-      template = ERB.new(File.read(TEMPLATE_CFG_FILE))
-      File.open(CFG_FILE, "w") { |cfg_file| cfg_file.puts template.result(binding) }
+        template = ERB.new(File.read(TEMPLATE_CFG_FILE))
+        File.open(CFG_FILE, "w") { |cfg_file| cfg_file.puts template.result(binding) }
 
-      `/etc/init.d/haproxy reload`
+        `/etc/init.d/haproxy reload`
 
-      puts "haproxy reloaded"
+        puts "haproxy reloaded"
+      end
+    rescue EOFError => ex
+        STDERR.puts "IO error when fetching cluster info"
     end
   end
 end
